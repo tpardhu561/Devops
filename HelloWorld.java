647aafce392da2b489a8f050cf18c374757298dd
Partha Saradhi Tatikonda <tpardhu561@gmail.com>
2017-06-22 01:57:13 -0400

    Fifth commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,14 +7,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println(3+6);
 		System.out.println("Second Commit");
 		System.out.println("Third commit");
 		System.out.println("So far fourth commit");
-		/*sc = new Scanner(System.in);
-		System.out.println("Enter a name : ");
-		String name = sc.next();
-		System.out.printf("Hello %s", name);*/
+		
 	}
 
 }
