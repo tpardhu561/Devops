248e3627c290549c9cc29b8d8dd17091ccbbef49
Pardhu <Pardhu@Pardhu_Spectre>
2017-06-28 13:48:55 -0400

    some changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 public class HelloWorld {
 
-	private static Scanner sc;
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
